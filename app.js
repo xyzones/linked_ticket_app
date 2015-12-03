8433fe42efacdc47b6efed1ef507b6b5fb94b5a2
Phil Grove <pgrove@zendesk.com>
2015-12-03 13:56:03 +1100

    refactor

--- app.js
+++ app.js
@@ -357,15 +357,16 @@
 
     copyDescription: function(){
       var useRichText = this.ticket().comment().useRichText();
+      var ticketDescription = this.ticket().description();
       var newLine = useRichText ? '<br />' : '\n';
       var descriptionDelimiter = helpers.fmt(newLine + "--- %@ ---" + newLine, this.I18n.t("delimiter"));
-      var currentDescription = this.formDescription()
+      var formDescription = this.formDescription()
         .split(descriptionDelimiter);
-     var ticketDescription = useRichText ? this.convertLineBreaksToHtml(this.ticket().description()) : this.ticket().description();
+     var ticketDescription = useRichText ? this.convertLineBreaksToHtml(ticketDescription) : ticketDescription;
 
-      var ret = currentDescription[0];
+      var ret = formDescription[0];
 
-      if (currentDescription.length === 1)
+      if (formDescription.length === 1)
         ret += descriptionDelimiter + ticketDescription;
 
       this.formDescription(ret);
